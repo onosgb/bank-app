d31e49b582a19bb2e3cfc555a463a5b41053ca98
Onosgb <onosgb@gmail.com>
2022-08-11 11:48:05 +0100

    added download button to the print

--- src/app/print/print.component.ts
+++ src/app/print/print.component.ts
@@ -1,5 +1,17 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Payment } from '../models/payment.model';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+declare var require: any;
+const htmlToPdfmake = require('html-to-pdfmake');
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-print',
@@ -12,5 +24,15 @@ export class PrintComponent {
 
   closePrint() {
     this.closePrintEvent.emit(true);
+  }
+
+  @ViewChild('downloadTable')
+  pdfDownload!: ElementRef;
+
+  public downloadAsPDF() {
+    const pdfDownload = this.pdfDownload.nativeElement;
+    var html = htmlToPdfmake(pdfDownload.innerHTML);
+    const documentDefinition = { content: html };
+    pdfMake.createPdf(documentDefinition).download();
   }
 }
